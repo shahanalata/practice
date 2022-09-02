40070cf4cbb5de9ddc14431b3b8da2f44aa47f68
shahanalata <shahanalata84@gmail.com>
2022-09-02 13:45:15 -0400

    'TestCase

--- src/main/java/com/SeleniumPractice/Bootstrap.java
+++ src/main/java/com/SeleniumPractice/Bootstrap.java
@@ -1,9 +1,11 @@
 package com.SeleniumPractice;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,14 +21,27 @@ public class Bootstrap {
 		driver.manage().timeouts().implicitlyWait(3000l, TimeUnit.SECONDS);
 		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
 		driver.findElement(By.xpath("//span[text()='HTML, CSS']")).click();
+List <WebElement> list = driver.findElements(By.xpath("//ul[contains(@class,'multiselect')]//li"));
 		
+		for (int i=0;i<list.size();i++) {
+			System.out.println(list.get(i).getText());
+//		list.get(i).click();;
+//			if (list.get(i).getText().equalsIgnoreCase("java")) {
+//				list.get(i).click();
+		}
+
+			if (list.get(i).getAttribute("class").equalsIgnoreCase("active")){
+				System.out.println(list.get(i).getText());
+			}else {
+				list.get(i).click();
+			}
 		
 		
 		
 		  Thread.sleep(3000);
 		  driver.quit();
 		
-
+		
 
 	}
 
---
+++ src/main/java/com/SeleniumPractice/TestCaseAutomation.java
@@ -0,0 +1,42 @@
+package com.SeleniumPractice;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCaseAutomation {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(6000l, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3000l, TimeUnit.SECONDS);//dynamic wait
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		WebElement ss = driver.findElement(By.xpath("//select[@id='dropdown-class-example']"));
+		Select select = new Select(ss);
+		Thread.sleep(3000l);
+		select.selectByVisibleText("Option2");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[@id='opentab']")).click();
+		Set<String>window=driver.getWindowHandles();
+		Iterator<String> it = window.iterator();
+		String parent = it.next();            //In Iterator class has 2 method-next(),Has next(use loop);
+		 String child = it.next();
+		 driver.switchTo().window(child);
+		 String childtitle = driver.getTitle();
+		 System.out.println(childtitle);
+		 Assert.assertEquals(childtitle,"Rahul Shetty Academy");
+		
+	}
+
+}
